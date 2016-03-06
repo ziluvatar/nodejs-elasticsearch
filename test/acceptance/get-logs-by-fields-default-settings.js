6f615ba7ad9adf4e646a6dcfcd6afff79229fe05
Eduardo Diaz <eduardo.ds@gmail.com>
2016-03-06 22:34:16 +0100

    Test refactoring

--- test/acceptance/get-logs-by-fields-default-settings.js
+++ test/acceptance/get-logs-by-fields-default-settings.js
@@ -22,12 +22,7 @@ describe('GET /logs?{fields}', function() {
   });
 
   it('returns all client entries default sorted (date descending) when searching without any query param', function(done) {
-    request(app)
-      .get('/logs')
-      .set('Accept', 'application/json')
-      .set('Authorization', 'Bearer ' + tokenBuilder.validToken())
-      .expect(200)
-      .expect('Content-Type', 'application/json; charset=utf-8')
+    basicGetRequest(app, '/logs')
       .end(function(err, res){
         expect(res.body).to.have.property('start').and.be.equal(0);
         expect(res.body).to.have.property('total').and.be.equal(3);
@@ -44,12 +39,7 @@ describe('GET /logs?{fields}', function() {
 
   it('returns first page when there are more results than default page size', function(done) {
     store.save([buildLogEntry()], function() {
-      request(app)
-        .get('/logs')
-        .set('Accept', 'application/json')
-        .set('Authorization', 'Bearer ' + tokenBuilder.validToken())
-        .expect(200)
-        .expect('Content-Type', 'application/json; charset=utf-8')
+      basicGetRequest(app, '/logs')
         .end(function(err, res){
           expect(res.body).to.have.property('start').and.be.equal(0);
           expect(res.body).to.have.property('total').and.be.equal(4);
@@ -66,12 +56,7 @@ describe('GET /logs?{fields}', function() {
   });
 
   it('returns client entries searching by username when it exists', function(done) {
-    request(app)
-      .get('/logs?user_name=user1')
-      .set('Accept', 'application/json')
-      .set('Authorization', 'Bearer ' + tokenBuilder.validToken())
-      .expect(200)
-      .expect('Content-Type', 'application/json; charset=utf-8')
+    basicGetRequest(app, '/logs?user_name=user1')
       .end(function(err, res){
         expect(res.body).to.have.property('start').and.be.equal(0);
         expect(res.body).to.have.property('total').and.be.equal(2);
@@ -86,12 +71,7 @@ describe('GET /logs?{fields}', function() {
   });
 
   it('does not return entries by username when there are not logs for that user', function(done) {
-    request(app)
-      .get('/logs?user_name=user3')
-      .set('Accept', 'application/json')
-      .set('Authorization', 'Bearer ' + tokenBuilder.validToken())
-      .expect(200)
-      .expect('Content-Type', 'application/json; charset=utf-8')
+    basicGetRequest(app, '/logs?user_name=user3')
       .end(function(err, res){
         expect(res.body).to.have.property('start').and.be.equal(0);
         expect(res.body).to.have.property('total').and.be.equal(0);
@@ -105,6 +85,16 @@ describe('GET /logs?{fields}', function() {
 
 
 });
+
+function basicGetRequest(app, url) {
+  return request(app)
+    .get(url)
+    .set('Accept', 'application/json')
+    .set('Authorization', 'Bearer ' + tokenBuilder.validToken())
+    .expect(200)
+    .expect('Content-Type', 'application/json; charset=utf-8');
+
+}
 
 function buildLogEntry(ext) {
   var extension = ext || {};
