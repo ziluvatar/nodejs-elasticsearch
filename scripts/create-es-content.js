ee669a97742962c552bc42fa722d70930f24965e
Eduardo Diaz <eduardo.ds@gmail.com>
2016-03-08 17:52:34 +0100

    Fix to create content scripts (add configured client id to some entries)

--- scripts/create-es-content.js
+++ scripts/create-es-content.js
@@ -10,6 +10,10 @@ var i;
 for (i = 0; i < logs.length; i++) {
   operations.push({delete: {_index: esIndex, _type: esType, _id: i + 1}});
   operations.push({index: {_index: esIndex, _type: esType, _id: i + 1}});
+
+  if (logs[i].client_id === "ok") {
+    logs[i].client_id = config.get('api.security.auth-client-id');
+  }
   operations.push(logs[i]);
 }
 
